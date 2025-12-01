18f70aaa262f61cd8f37b46ce3d4ab2acb4583c0
vitorgabriel27 <vitor.alves@geowellex.com>
2025-11-30 23:54:49 -0300

    fix: update objective function

--- project/core/fitness/fitness_functions.py
+++ project/core/fitness/fitness_functions.py
@@ -6,5 +6,5 @@ def objective_function(pos):
     z = -x * np.sin(np.sqrt(np.abs(x))) - y * np.sin(np.sqrt(np.abs(y)))
     x_norm = x / 250
     y_norm = y / 250
-    r = 100 * (y_norm - x_norm * 2) ** 2 + (1 - x_norm) ** 2
+    r = 100 * (y_norm - x_norm ** 2) ** 2 + (1 - x_norm) ** 2
     return r - z
